6efc04d155b8b0bebbaa57d635221e81fc4020f7
Dan Van Boxel <the.dvb@gmail.com>
2015-06-19 22:55:00 -0400

    Basic exhaustion of a simple problem
    Keeps significant variables and stores scores.

--- ee.py
+++ ee.py
@@ -1,5 +1,6 @@
 import rpy2.robjects as r
 import numpy as np
+import itertools
 
 # evaluate some r code
 r.r['pi']
@@ -54,6 +55,12 @@ def get_pvals(summ):
     pvals = coeff_arr[:,p_col]
     return pvals
 
+def get_aic(summ):
+    '''Get the Akaike Information Criterion value from the model summary'''
+    aic_idx = summ.names.index('aic')
+    aic = summ[aic_idx][0]
+    return aic
+
 model_pvals = get_pvals(summ_res)
 # note that an intercept is the first one, will have to check which var is where
 # have to handle the intercept special and explicitly turn it off
@@ -64,4 +71,28 @@ model_int = model_func(formula = test_int, data = data)
 summ_int = rsumm(model_int)
 int_pvals = get_pvals(summ_int)
 
+# stupid exhauster, don't pay attention to anything but pvals, keep only significant sets
+def check_model(dep_var, ivars, data, model = model_func):
+    '''Model dep_var using the ivars and model_func.  Return the probabilities of the variable coefficients being random and some score (like AIC)'''
+    form = build_formula(dep_var, ivars)
+    m = model(formula = form, data = data)
+    summary = rsumm(m)
+    return get_pvals(summary), get_aic(summary)
+
+# typical academic threshold for significance
+thresh = 0.05
+dep_var = var_names[1]
+ind_vars = list(var_names[5:9]) + ['1']
+
+debug = True
+
+good_models = []
+for num_vars in range(1,len(ind_vars)):
+    for ivars in itertools.combinations(ind_vars,num_vars):
+        if debug:
+            print(ivars)
+        var_probs, score = check_model(dep_var, ivars, data)
+        # if all vars are significant, add to pile of good models
+        if True not in map(lambda x: x > thresh, var_probs): 
+            good_models.append((ivars,var_probs,score)) 
 
